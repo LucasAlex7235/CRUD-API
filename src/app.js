6bfe849b16e927bde547b4bc7e08f63888b57f0f
Lucas <lucasalex7235@gmail.com>
2022-11-24 15:17:44 -0300

    add delete and patch user

--- src/app.js
+++ src/app.js
@@ -24,37 +24,11 @@ const createUserService = async (email, name, password, isAdm) => {
     password: hashedPassword,
     isAdm,
   };
-
   users.push(newUser);
 
-  return [201, newUser];
-};
+  const { uuid, createdOn, updatedOn } = newUser;
 
-const userLoginService = async (email, password) => {
-  const user = users.find((element) => element.email === email);
-
-  if (!user) {
-    return [401, { message: "Email ou senha inválidos" }];
-  }
-
-  const passwordMatch = await compare(password, user.password);
-
-  if (!passwordMatch) {
-    return [401, { message: "Email ou senha inválidos" }];
-  }
-
-  const token = jwt.sign({ email }, process.env.SECRET_KEY, {
-    expiresIn: "24h",
-    subject: user.uuid,
-  });
-
-  return [200, { token: token }];
-};
-
-const retrieveUserService = (request) => {
-  const authToken = request.headers.authorization;
-  const { uuid, createdOn, updatedOn, name, email, isAdm } = users[0];
-  const newUser = {
+  const userWithoutPassword = {
     uuid: uuid,
     createdOn: createdOn,
     updatedOn: updatedOn,
@@ -63,30 +37,84 @@ const retrieveUserService = (request) => {
     isAdm: isAdm,
   };
 
-  if (!authToken) {
-    return [401, { message: "Missing authorization token." }];
-  }
-
-  const token = authToken.split(" ")[1];
-
-  const [status, user] = verify(
-    token,
-    process.env.SECRET_KEY,
-    (error, decoded) => {
-      if (error) {
-        return [401, { message: error.message }];
-      }
-      return [200, newUser];
-    }
-  );
-
-  return [status, user];
+  return [201, userWithoutPassword];
 };
 
-const userUpdadeService = (index, body) => {
-  const newUser = { ...users[index], ...body, updatedOn: new Date() };
-  users[index] = newUser;
-  return [204, newUser];
+const userLoginService = async ({ token }) => {
+  return [200, { token: token }];
+};
+
+const retrieveUserService = ({
+  uuid,
+  createdOn,
+  updatedOn,
+  name,
+  email,
+  isAdm,
+}) => {
+  const userProfile = {
+    uuid: uuid,
+    createdOn: createdOn,
+    updatedOn: updatedOn,
+    name: name,
+    email: email,
+    isAdm: isAdm,
+  };
+
+  return [200, userProfile];
+};
+
+const listUserService = ({ isAdm }) => {
+  if (isAdm) {
+    const listUsersNotPassword = users.map((user) => {
+      const newUser = {
+        uuid: user.uuid,
+        createdOn: user.createdOn,
+        updatedOn: user.updatedOn,
+        name: user.name,
+        email: user.email,
+        isAdm: user.isAdm,
+      };
+      return newUser;
+    });
+
+    return [200, listUsersNotPassword];
+  }
+  return [403, { message: "missing admin permissions" }];
+};
+
+const userUpdadeService = ({ params, user, body }) => {
+  const { id } = params;
+  const { uuid, isAdm } = user;
+  const userIndex = users.findIndex((user) => user.uuid === id);
+
+  if (userIndex === -1) {
+    return [404, { message: "User not found" }];
+  } else if (isAdm) {
+    const newUser = {
+      uuid: body.uuid ? body.uuid : user.uuid,
+      createdOn: body.createdOn ? body.createdOn : user.createdOn,
+      updatedOn: body.updatedOn ? body.updatedOn : user.updatedOn,
+      name: body.name ? body.name : user.name,
+      email: body.email ? body.email : user.email,
+      isAdm: body.isAdm ? body.isAdm : user.isAdm,
+    };
+    const updateUser = { ...users[userIndex], ...body, updatedOn: new Date() };
+    users[userIndex] = updateUser;
+    return [200, newUser];
+  } else if (!isAdm && uuid === id) {
+    const newUser = {
+      uuid: body.uuid ? body.uuid : user.uuid,
+      createdOn: body.createdOn ? body.createdOn : user.createdOn,
+      updatedOn: body.updatedOn ? body.updatedOn : user.updatedOn,
+      name: body.name ? body.name : user.name,
+      email: body.email ? body.email : user.email,
+      isAdm: body.isAdm ? body.isAdm : user.isAdm,
+    };
+    const updateUser = { ...users[userIndex], ...body, updatedOn: new Date() };
+    users[userIndex] = updateUser;
+    return [200, newUser];
+  }
 };
 
 const userDeleteService = (index) => {
@@ -110,31 +138,90 @@ const verifyEmailExistsMiddleware = (request, response, next) => {
   return next();
 };
 
-const verifyIsAdmPath = (request, response, next) => {
+const verifyUserExist = async (request, response, next) => {
+  const { email, password } = request.body;
+
+  const user = users.find((element) => element.email === email);
+
+  if (!user) {
+    return response.status(401).json({
+      message: "Email ou senha inválidos",
+    });
+  }
+
+  const passwordMatch = await compare(password, user.password);
+
+  if (!passwordMatch) {
+    return response.status(401).json({
+      message: "Email ou senha inválidos",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      name: user.name,
+      email: user.email,
+      isAdm: user.isAdm,
+      createdOn: user.createdOn,
+      updatedOn: user.updatedOn,
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "24h",
+      subject: user.uuid,
+    }
+  );
+
+  request.token = token;
+
+  return next();
+};
+
+const authorizationVerify = (request, response, next) => {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    return response
+      .status(401)
+      .json({ message: "Missing authorization token." });
+  }
+  const token = authToken.split(" ")[1];
+
+  return verify(token, process.env.SECRET_KEY, (error, decoded) => {
+    if (error) {
+      return response.status(401).json({ message: error.message });
+    }
+
+    request.user = {
+      uuid: decoded.sub,
+      name: decoded.name,
+      email: decoded.email,
+      isAdm: decoded.isAdm,
+      createdOn: decoded.createdOn,
+      updatedOn: decoded.updatedOn,
+    };
+
+    return next();
+  });
+};
+
+const verifyIsAdm = (request, response, next) => {
   const id = request.params.id;
-  const [status, user] = retrieveUserService(request);
+  const { isAdm, uuid } = request.user;
 
-  if (status >= 400) {
-    return response.status(status).json(user);
+  if (!isAdm && uuid !== id) {
+    return response.status(403).json({
+      message: "missing admin permissions",
+    });
   }
-
-  const userUuidExist = users.find((element) => element.uuid === id);
-  const userIndex = users.findIndex((element) => element.uuid === id);
-
-  if (!userUuidExist) {
-    return response.status(404).json("User not found");
-  } else if (!userUuidExist.isAdm) {
-    return response.status(403).json({ message: "missing admin permissions" });
-  }
-
-  request.userIndex = userIndex;
 
   return next();
 };
 
 // Controllers
 const listUserController = (request, response) => {
-  return response.status(200).json(users);
+  const [status, data] = listUserService(request.user);
+  return response.status(status).json(data);
 };
 
 const createUserController = async (request, response) => {
@@ -146,43 +233,43 @@ const createUserController = async (request, response) => {
 };
 
 const userLoginController = async (request, response) => {
-  const { email, password } = request.body;
-
-  const [status, token] = await userLoginService(email, password);
-
+  const [status, token] = await userLoginService(request);
   return response.status(status).json(token);
 };
 
 const retrieveUserController = (request, response) => {
-  const [status, user] = retrieveUserService(request);
+  const [status, user] = retrieveUserService(request.user);
   return response.status(status).json(user);
 };
 
 const userUpdadeController = (request, response) => {
-  const i = request.userIndex;
-  const [status, user] = userUpdadeService(i, request.body);
+  const [status, user] = userUpdadeService(request);
   return response.status(status).json(user);
 };
 
 const userDeleteController = (request, response) => {
   const i = request.userIndex;
-
   const [status, message] = userDeleteService(i);
 
   return response.status(status).json(message);
 };
 
-app.get("/users", listUserController);
+app.get("/users", authorizationVerify, listUserController);
 
 app.post("/users", verifyEmailExistsMiddleware, createUserController);
 
-app.post("/login", userLoginController);
+app.post("/login", verifyUserExist, userLoginController);
 
-app.get("/users/profile", retrieveUserController);
+app.get("/users/profile", authorizationVerify, retrieveUserController);
 
-app.patch("/users/:id", verifyIsAdmPath, userUpdadeController);
+app.patch("/users/:id", authorizationVerify, verifyIsAdm, userUpdadeController);
 
-app.delete("/users/:id", verifyIsAdmPath, userDeleteController);
+app.delete(
+  "/users/:id",
+  authorizationVerify,
+  verifyIsAdm,
+  userDeleteController
+);
 
 app.listen(PORT, () => {
   console.log(`App rodando na porta ${PORT}`);
